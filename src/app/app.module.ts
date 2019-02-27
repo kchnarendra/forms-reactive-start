37a4e12df73d67afc032d5c37125271fa673a60c
Narendra Chaudhary <>
2019-02-28 02:02:04 +0530

    Your Message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//import { FormsModule } from '@angular/forms'  //is for only template forms
+import { ReactiveFormsModule } from '@angular/forms'; //for reactive forms
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
